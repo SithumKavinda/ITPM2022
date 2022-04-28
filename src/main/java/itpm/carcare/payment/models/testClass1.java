cdeb696818aa717b758122c4d8ddb5c379846c4c
sithumkavinda <sithumkavinda@localhost>
2022-04-28 17:16:45 +0530

    Test class added to test the connection between local and remote

---
+++ src/main/java/itpm/carcare/payment/models/testClass1.java
@@ -0,0 +1,12 @@
+package itpm.carcare.payment.models;
+
+public class testClass1 {
+	
+	private String name;
+	
+	public String nameout(String nameText) {
+		this.name = nameText;
+		return nameText;
+	}
+	
+}
