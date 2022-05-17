6cee1ee11ca2b9a9e44307757071ca015d56ea1c
Perera D T W S <it20003746@my.sliit.lk>
2022-05-17 15:31:19 +0530

    Java Class

--- EmployeeManagement02/src/main/java/com/it20003746/employeemanagement/model/Employee.java
+++ EmployeeManagement02/src/main/java/com/it20003746/employeemanagement/model/Employee.java
@@ -10,8 +10,6 @@ public class Employee {
 	private int contact;
 	
 	
-	
-	
 	public Employee(int id, String name, String gender, String dob, String email, int contact) {
 		super();
 		this.id = id;
@@ -22,58 +20,3 @@ public class Employee {
 		this.contact = contact;
 	}
 	
-	
-	
-	
-	public Employee(String name, String gender, String dob, String email, int contact) {
-		super();
-		this.name = name;
-		this.gender = gender;
-		this.dob = dob;
-		this.email = email;
-		this.contact = contact;
-	}
-
-
-
-
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getGender() {
-		return gender;
-	}
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-	public String getDob() {
-		return dob;
-	}
-	public void setDob(String dob) {
-		this.dob = dob;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public int getContact() {
-		return contact;
-	}
-	public void setContact(int contact) {
-		this.contact = contact;
-	}
-	
-	
-
-}
