94c104c98cae8bb4c7ddf7332b770eab776d0217
Perera D T W S <it20003746@my.sliit.lk>
2022-05-17 15:35:55 +0530

    java class commit

--- EmployeeManagement02/src/main/java/com/it20003746/employeemanagement/model/Employee.java
+++ EmployeeManagement02/src/main/java/com/it20003746/employeemanagement/model/Employee.java
@@ -28,3 +28,46 @@ public class Employee {
 		this.email = email;
 		this.contact = contact;
 	}
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getContact() {
+		return contact;
+	}
+	public void setContact(int contact) {
+		this.contact = contact;
+	}
+	
+	
+
+}
+	
